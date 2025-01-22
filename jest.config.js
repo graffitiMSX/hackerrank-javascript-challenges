d4acba05b58e39cfe568295f6374f42d14c0ca72
Rudolf Gutlich <rudolfgutlich@gmail.com>
2025-01-21 23:22:23 -0300

    adjust jest setup

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ['js', 'json'],
-  rootDir: '.',
-  testRegex: '.*\\.spec\\.js$',
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
-  testEnvironment: 'node',
-}
